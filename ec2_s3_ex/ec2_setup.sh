6ef1f5024a92bd6f92da63e51d2097decdc8a99f
Anson Chan <ansonchan100@gmail.com>
2022-05-09 16:11:18 +0100

    Fixed typo

--- ec2_s3_ex/ec2_setup.sh
+++ ec2_s3_ex/ec2_setup.sh
@@ -7,5 +7,5 @@ sudo apt install python -y
 sudo apt install python3-pip -y
 alias python=python3
 export python=python3
-sudo python3 -m pip install awscli
+sudo python3 -m pip install awscli -y
 pip3 install boto3
